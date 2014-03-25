de2680e01d7f968e5775725fab361629a2bf739b
Sam Yim <syim@one-k.com>
2014-03-25 10:29:46 -0700

    Add project

---
+++ DemoProject/DemoProject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DemoProject
+//
+//  Created by Sam Yim on 3/25/14.
+//  Copyright (c) 2014 OneK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
